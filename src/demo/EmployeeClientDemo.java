01e42b9a02904592e35bb0654e4f41edb37338a1
amin <amin@MI6>
2017-10-10 00:07:26 -0400

    Added demo for employee client.

---
+++ src/demo/EmployeeClientDemo.java
@@ -0,0 +1,71 @@
+package demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import client.EmployeeClient;
+import core.Employee;
+import core.PhoneNumber;
+
+public class EmployeeClientDemo {
+	public static void main(String[] args) {
+		EmployeeClient client = new EmployeeClient();
+
+		System.out.println("TEST: readAll");
+		List<Employee> employees = client.readAll();
+
+		for (Employee e : employees) {
+			System.out.println(e);
+		}
+
+		Employee employee = new Employee();
+		employee.setCedula("05800000007");
+		employee.setFirstName("James");
+		employee.setFirstSurname("Bond");
+		employee.setEmail("bond.james@sis.gov.uk");
+		employee.setRole(Employee.Role.SUPPORT);
+		employee.setPhoneNumbers(new ArrayList<PhoneNumber>() {
+			private static final long serialVersionUID = 1L;
+
+			{
+				add(new PhoneNumber("0800789321", PhoneNumber.Type.WORK));
+			}
+			{
+				add(new PhoneNumber("0800789322", PhoneNumber.Type.HOME));
+			}
+		});
+
+		System.out.println("TEST: create");
+		client.create(employee);
+
+		employees = client.readAll();
+
+		for (Employee e : employees) {
+			System.out.println(e);
+		}
+
+		employee = client.readByUniqueIdentifier("cedula", "05800000007");
+		employee.setMiddleName("Herbert");
+
+		System.out.println("TEST: update");
+		client.update(employee.getEntityId(), employee);
+
+		employees = client.readAll();
+
+		for (Employee e : employees) {
+			System.out.println(e);
+		}
+
+		System.out.println("TEST: delete");
+		client.delete(employee.getEntityId());
+
+		employees = client.readAll();
+
+		for (Employee e : employees) {
+			System.out.println(e);
+		}
+
+		client.close();
+
+	}
+}
